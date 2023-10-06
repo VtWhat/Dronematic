aea9be93ccf76716740f100082e16e261ae23d3a
João Vitor Helm <jvthelms@gmail.com>
2023-10-06 13:06:44 -0300

    Criação das tabelas no banco de dados para clientes e serviços. Páginas de exibir clientes e serviços adicionadas.

--- app/exibir/servicos/page.tsx
+++ app/exibir/servicos/page.tsx
@@ -8,7 +8,7 @@ import { redirect } from 'next/navigation'
 
 export const dynamic = 'force-dynamic'
 
-export default async function Home() {
+export default async function ExibirServicos() {
   const supabase = createServerComponentClient({ cookies })
 
   const {
@@ -30,31 +30,28 @@ export default async function Home() {
           )}
         </div>
       </nav>
-        Página de menu com opções
+        Exibição de serviços cadastrados para usuário autenticado
+
         <Link
-          href="/cadastro/cliente"
-          className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover text-white"
+        href="/home"
+        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
         >
-          Cadastrar Cliente
-        </Link>
-        <Link
-          href="/cadastro/servico"
-          className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover text-white"
-        >
-          Cadastrar Serviço
-        </Link>
-        <Link
-          href="/exibir/clientes"
-          className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover text-white"
-        >
-          Ver Clientes
-        </Link>
-        <Link
-          href="/exibir/servicos"
-          className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover text-white"
-        >
-          Ver Serviços
-        </Link>
+          <polyline points="15 18 9 12 15 6" />
+        </svg>{' '}
+        Voltar
+      </Link>
       
     </div>
   )
