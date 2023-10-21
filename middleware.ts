89ba8a1504e5a90aeab0ec836699cd4692e2fb3c
João Vitor Helm <jvthelms@gmail.com>
2023-10-20 23:15:12 -0300

    Updated middleware

--- middleware.ts
+++ middleware.ts
@@ -18,6 +18,11 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL('/login', req.url))
   }
 
+  // if user is signed in and path is / - redirect  user to /home
+  if (user && req.nextUrl.pathname == '/') {
+    return NextResponse.redirect(new URL('/home', req.url))
+  }
+
   // Refresh session if expired - required for Server Components
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
   await supabase.auth.getSession()
@@ -29,7 +34,7 @@ export const config = {
   matcher: [
     '/',
     '/home',
-    '/cadastro/:path*',
-    '/exibir/:path*',
+    '/customer/:path*',
+    '/service/:path*',
   ],
 }
